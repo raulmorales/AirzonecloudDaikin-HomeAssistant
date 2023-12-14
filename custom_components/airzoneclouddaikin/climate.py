1c31022143fa961f59f3e1e2b984731a56ef28be
Raul <rmorales@atsistemas.com>
2023-12-14 08:32:47 +0100

    Fix deprectated temperature utility (replaced by unit_conversion.TemperatureConverter)

--- custom_components/airzoneclouddaikin/climate.py
+++ custom_components/airzoneclouddaikin/climate.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Dict, List, Optional
 from datetime import timedelta
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
-from homeassistant.util.temperature import convert as convert_temperature
+from homeassistant.util.unit_conversion import TemperatureConverter
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
@@ -166,14 +166,14 @@ class AirzonecloudDaikinDevice(ClimateEntity):
     @property
     def min_temp(self) -> float:
         """Return the minimum temperature."""
-        return convert_temperature(
+        return TemperatureConverter.convert(
             self._azc_device.min_temperature, TEMP_CELSIUS, self.temperature_unit
         )
 
     @property
     def max_temp(self) -> float:
         """Return the maximum temperature."""
-        return convert_temperature(
+        return TemperatureConverter.convert(
             self._azc_device.max_temperature, TEMP_CELSIUS, self.temperature_unit
         )
 
